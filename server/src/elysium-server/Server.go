b54e167431a184c6dd27e9434a1ed816e0330d9c
Matt Owen <not.mattowen@gmail.com>
2016-11-06 13:59:26 -0500

    :fish_cake:

--- server/src/elysium-server/Server.go
+++ server/src/elysium-server/Server.go
@@ -16,8 +16,23 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Ayy")
 }
 
+func adrilonHandler(w http.ResponseWriter, r *http.Request) {
+	conn, _ := upgrader.Upgrade(w, r, nil)
+
+	client := Client{
+		ws:    conn,
+		send:  make(chan []byte),
+		world: make(chan string),
+	}
+
+	clients <- client
+}
+
+var upgrader = websocket.Upgrader{}
+var clients = make(chan Client)
+
 func main() {
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/Adrilon", indexHandler)
+	http.HandleFunc("/Adrilon", adrilonHandler)
 	http.ListenAndServe(":8080", nil)
 }
