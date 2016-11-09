2e327af648e4dfc92ab24d988a007381e04a0435
Matt Owen <not.mattowen@gmail.com>
2016-11-09 17:13:16 -0500

    :cake:

--- server/src/elysium-server/Server.go
+++ server/src/elysium-server/Server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
+	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -12,27 +14,52 @@ type Client struct {
 	world chan string
 }
 
+// Index Handler
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Ayy")
+
+	type Page struct {
+	}
+
+	p := &Page{}
+	t, err := template.ParseFiles("pages/index.html")
+
+	if err == nil {
+		log.Print("Writing index.html out")
+		t.Execute(w, p)
+	} else {
+		log.Fatal("Cannot find pages/index.html")
+		fmt.Fprintf(w, "Failure")
+	}
 }
 
 func adrilonHandler(w http.ResponseWriter, r *http.Request) {
 	conn, _ := upgrader.Upgrade(w, r, nil)
 
+	log.Print("Creating new client")
 	client := Client{
 		ws:    conn,
 		send:  make(chan []byte),
 		world: make(chan string),
 	}
 
-	clients <- client
+	log.Print("Adding client to world")
+	world.clients <- client
+}
+
+// Globals... can we avoid this?
+
+var world = Zone{
+	clients: make(chan Client),
 }
 
 var upgrader = websocket.Upgrader{}
-var clients = make(chan Client)
 
 func main() {
+
+	fmt.Println("whatever")
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/Adrilon", adrilonHandler)
+
+	fmt.Println("Start server")
 	http.ListenAndServe(":8080", nil)
 }
---
+++ server/src/elysium-server/Zone.go
@@ -0,0 +1,5 @@
+package main
+
+type Zone struct {
+	clients chan Client
+}
