bd18e37e54fb1d3430b573e1f823b2a5f4d9532d
Matt Owen <not.mattowen@gmail.com>
2016-11-15 00:38:19 -0500

    :wolf:

--- server/src/elysium-server/Zone.go
+++ server/src/elysium-server/Zone.go
@@ -1,5 +1,25 @@
 package main
 
+type Point struct {
+	x int
+	y int
+}
+
+type Hexagon struct {
+	nw Point
+	ne Point
+	e  Point
+	se Point
+	sw Point
+	w  Point
+
+	//    NW-----NE
+	//    /       \
+	//   W         E
+	//    \       /
+	//    SW-----SE
+}
+
 type Zone struct {
 	clients chan Client
 	actions chan Action
@@ -11,4 +31,12 @@ type Zone struct {
 	south     *Zone
 	southwest *Zone
 	northwest *Zone
+
+	// Boundaries
+	boundaries Hexagon
+	//     0-----1
+	//    /       \
+	//   5         2
+	//    \       /
+	//     4-----3
 }
